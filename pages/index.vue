e6b1383b2608a2b2c44a2c5470ac392a06b8cf4f
Nano <fernandoheredia33@gmail.com>
2026-02-17 20:07:30 +0100

    Initial commit: RRHH monorepo (BACK + FRONT)

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div style="font-family: system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial; display:flex; align-items:center; justify-content:center; height:100vh;">
+    <div style="text-align:center">
+      <h1>RRHH — Nuxt 3 (root en FRONT/)</h1>
+      <p>Servidor de desarrollo disponible en <code>http://localhost:3000</code></p>
+    </div>
+  </div>
+</template>
